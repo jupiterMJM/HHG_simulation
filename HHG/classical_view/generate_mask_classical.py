eb5fce482506143017441f86aef055d9ac13bf5a
Maxence B. <maxence.barre59@gmail.com>
2025-06-24 11:06:14 +0200

    change of the mask: we just plot (not create a full mask matrix)

--- HHG/classical_view/generate_mask_classical.py
+++ HHG/classical_view/generate_mask_classical.py
@@ -15,69 +15,6 @@ from tqdm import tqdm
 
 
 
-############################################################################################
-## PURE PLOTTING FUNCTIONS
-## in order to go from curves to a matrix of mask
-############################################################################################
-
-def blend_pixel_matrices(matrix1:np.array, matrix2:np.array) -> np.array:
-    """
-    Blends two matrices pixel by pixel, returning a new matrix where each pixel is taken from matrix2 if it is non-zero,
-    non-NaN, and not None; otherwise, it takes the pixel from matrix1.
-    :param matrix1: First input matrix.
-    :param matrix2: Second input matrix.
-    :return: Blended matrix.
-    """
-    retour = np.where(np.logical_and(matrix2 != 0, matrix2 != None), matrix2, matrix1)
-    return np.array(retour, dtype=float)
-
-
-
-
-
-
-def genere_array_modified(x_grid, y_grid, x, value):
-    """
-    Generate a binary matrix where only the closest elements to the curve defined by value are set to 1.
-
-    Parameters:
-    x_grid (numpy.ndarray): Grid of x values.
-    y_grid (numpy.ndarray): Grid of y values.
-    value (numpy.ndarray): Precomputed values of the function f(x) corresponding to x_grid.
-
-    Returns:
-    numpy.ndarray: Binary matrix with 1s for points closest to the curve.
-    """
-    # Create meshgrid and compute grid points
-    X_grid, Y_grid = np.meshgrid(x_grid, y_grid)
-    grid_points = np.column_stack((X_grid.ravel(), Y_grid.ravel()))
-
-    # Compute curve points using the precomputed value
-    # print(x.shape, value.shape)
-    curve_points = np.column_stack((x, value))
-
-    # Calculate distances from grid points to curve points
-    distances = cdist(grid_points, curve_points)
-
-    # Find the minimum distance for each grid point
-    min_distances = distances.min(axis=1)
-
-    # Determine the closest points to the curve
-    closest_indices = np.argmin(distances, axis=0)
-
-    # Initialize the matrix with zeros
-    matrix = np.zeros_like(X_grid, dtype=bool)
-
-    # Set the closest grid points to True (1)
-    for idx in closest_indices:
-        # print(idx)
-        matrix.flat[idx] = True
-
-    # matrix[matrix == False] = None
-
-    return matrix
-
-###############################################################################
 
 
 
@@ -102,7 +39,7 @@ def integration_laws_of_motion(t, E_laser, x0=0, v0=0):
         :return: derivative of the state of the system, a vector [velocity, acceleration]
         """
         dxdt = x[1]  # velocity
-        dvdt = - e * E_laser(t) / m_e  # acceleration, F = m*a, with F = -e*E_laser 
+        dvdt = -E_laser(t) # acceleration, F = m*a, with F = -e*E_laser in AU units (e=1, m=1 in AU)
         return [dxdt, dvdt]
     
     return solve_ivp(dXdt, t_span = [t[0], t[-1]], y0=[x0, v0], t_eval=t)
@@ -112,124 +49,90 @@ def integration_laws_of_motion(t, E_laser, x0=0, v0=0):
 ################################################################################
 ## MAIN FUNCTION
 ################################################################################
-def generate_mask_from_classical(time_grid, position_grid, E_field, time_grid_zoomed, position_grid_zoomed):
+def plot_classical_trajectories(E_laser, time_grid, ax, time_grid_for_plot, position_grid_for_plot):
+    """ this function plots the classical trajectories of the electron in the laser field
+    :param E_laser: laser field in V/m (function of time)
+    :param time_grid: time grid in s (array)
+    :param ax: axis to plot on
+    :param time_grid_for_plot: time grid for plotting in fs
+    :param position_grid_for_plot: position grid for plotting in a.u.
     """
-    Generate a mask from classical trajectories based on the electric field.
+    norm = mcolors.Normalize(vmin=0, vmax=1)
+    cmap = cm.turbo
+    for time_ionization in tqdm(time_grid[time_grid<time_grid_for_plot[-1]][::20], desc="Integrating classical trajectories"):
+        # integrate the equations of motion
+        time_to_integrate_on = time_grid[time_grid>=time_ionization]
+        result = integration_laws_of_motion(time_to_integrate_on, E_laser, x0=0, v0=0)
+        # get the position and velocity of the electron
+        position = result.y[0]
+        velocity = result.y[1]
 
-    Parameters:
-    time_grid (numpy.ndarray): Grid of time values. IN SECONDS
-    position_grid (numpy.ndarray): Grid of position values. IN METERS
-    E_field (function): Function representing the electric field as a function of time.
-    Returns:
-    numpy.ndarray: Final mask representing the classical trajectories.
-    """
-    # cmap = cm.turbo
-    norm = mcolors.Normalize(vmin=0, vmax=10)
-    retour = np.zeros((len(position_grid_zoomed), len(time_grid_zoomed)), dtype=float)
-    # retour[retour==0] = None  # Initialize with None for empty pixels
-    # alpha_retour = .5
+        if np.all(position[1:] * position[1] > 0):  # if the electron is always on the same side of the ion
+            # the electron is not recombining with the ion
+            time_out_of_scope = t[np.where(np.logical_and(position > position_grid_for_plot[0], position < position_grid_for_plot[-1]))][-1]
+            # print(time_out_of_scope, time_grid_for_plot[0])
+            if time_out_of_scope < time_grid_for_plot[0]:
+                # if the last time is before the plot range, skip this ionization time
+                continue
+            end_of_plot = min(time_grid_for_plot[-1], time_out_of_scope)
+            position = position[np.logical_and(time_to_integrate_on >= time_grid_for_plot[0], time_to_integrate_on <= end_of_plot)]
+            time_to_plot = time_to_integrate_on[np.logical_and(time_to_integrate_on >= time_grid_for_plot[0], time_to_integrate_on <= end_of_plot)]
+            np.clip(position, position_grid_for_plot[0], position_grid_for_plot[-1], out=position)
+            ax.plot(time_to_plot, position, alpha=0.5, color='lightblue', label=f'Ionization at {time_ionization:.2f} fs')  # convert x to nm for plotting
 
-    for time_ionization in tqdm(time_grid[1:]):
-        # print(f"Processing ionization time: {time_ionization} s")
-        t = time_grid[time_grid >= time_ionization]
-
-        # Integrate the equations of motion for the electron
-        result = integration_laws_of_motion(t, E_field, x0=0, v0=0)
-        # print(f"Integration result got")
-        if not result.success or len(result.y) == 0:
-            # print(f"Integration failed for time {time_ionization}. Skipping this trajectory.")
-            continue
-        # Extract position and velocity
-        x = result.y[0]
-        v = result.y[1]
-
-        if np.all(x[1:] * x[1] > 0): # if the electron is always on the same side of the ion
-            # alpha = 0.3 # transparency for the mask
-            color = norm(0.1)
         else:
-            
-            t_recombination = t[np.where(np.diff(np.sign(x[1:])))[0][0]]  # find the time of recombination
-            x = x[t <= t_recombination]  # only keep the position before recombination
-            v_recomb = v[t== t_recombination][0]  # velocity at recombination
-            t = t[t <= t_recombination]  # only keep the time before recombination
-            
-            energy_recomb = 0.5 * m_e * v_recomb**2/e  # kinetic energy at recombination
+            # print(time_to_integrate_on.shape, np.where(np.diff(np.sign(position[1:])))[0][0])
+            t_recombination = time_to_integrate_on[np.where(np.diff(np.sign(position[1:])))[0][0]]  # find the time of recombination
 
-            color = norm(energy_recomb)+0.1 # TODO modify
-            print(f"Electron recombining at time {time_ionization} s, energy at recombination: {energy_recomb:.2e} J, velocity: {v_recomb:.2e} m/s, color: {color:.2f}")
-            # alpha = 0.5
-        
-        # plt.figure()
-        # plt.plot(t, x, label=f'Ionization at {time_ionization:.2f} fs')
+            # print(t_recombination, velocity[time_to_integrate_on == t_recombination])
+            position = position[time_to_integrate_on <= t_recombination]  # only keep the position before recombination
+            velocity_recomb = velocity[time_to_integrate_on == t_recombination][0]
+            time_to_integrate_on = time_to_integrate_on[time_to_integrate_on <= t_recombination]  # only keep the time before recombination
 
-        # t = t[np.logical_and(x >= position_grid_zoomed[0], x <= position_grid_zoomed[-1])]
-        # x = x[np.logical_and(x >= position_grid_zoomed[0], x <= position_grid_zoomed[-1])]
-        # print(t)
-        x = x[np.logical_and(t >= time_grid_zoomed[0], t <= time_grid_zoomed[-1])]
-        t = t[np.logical_and(t >= time_grid_zoomed[0], t <= time_grid_zoomed[-1])]
-        if len(t) == 0 or len(x) == 0:
-            # print(f"No valid trajectory points found for time {time_ionization}. Skipping this trajectory.")
-            continue
-        # print(alpha, color)
-        # print(t, x)
-        # plt.figure()
-        # plt.scatter(t, x, color=color[:3], alpha=alpha, s=1, label=f'Ionization at {time_ionization:.2f} fs')
-        # plt.show()
+            if time_to_integrate_on[-1] < time_grid_for_plot[0] or time_to_integrate_on[0] > time_grid_for_plot[-1]:
+                # if the recombination time is outside the plot range, skip this ionization time
+                continue
 
-        # Create a mask for the current trajectory
-        mask = genere_array_modified(time_grid_zoomed, position_grid_zoomed, t, x)
-        # plt.figure()
-        # plt.imshow(mask, cmap="grey", aspect='auto', extent=(time_grid_zoomed[0], time_grid_zoomed[-1], position_grid_zoomed[0], position_grid_zoomed[-1]))
 
-        mask_color = np.zeros((len(position_grid_zoomed), len(time_grid_zoomed)), dtype=float)
-        mask_color[mask] = color
-        # mask_color[~mask] = 0
-        # plt.figure()
-        # plt.imshow(mask_color, aspect='auto', extent=(time_grid_zoomed[0], time_grid_zoomed[-1], position_grid_zoomed[0], position_grid_zoomed[-1]), cmap='grey')
-        # plt.title(f"Mask for ionization at {time_ionization:.2f} s")
-        # plt.colorbar()
-        # # plt.show()
-        # Blend the mask with the existing matrix
-
-        # plt.figure()
-        # plt.imshow(retour, aspect='auto', extent=(time_grid_zoomed[0], time_grid_zoomed[-1], position_grid_zoomed[0], position_grid_zoomed[-1]), cmap='grey')
-        # plt.title("Current state of the mask")
-        # plt.colorbar()
-        # plt.show()
-
-        retour = blend_pixel_matrices(retour, mask_color)
-        # plt.figure()
-        # plt.imshow(retour, aspect='auto', extent=(time_grid_zoomed[0], time_grid_zoomed[-1], position_grid_zoomed[0], position_grid_zoomed[-1]), cmap='grey')
-        # plt.title("Blended mask after processing current trajectory")
-        # plt.colorbar()
-        # plt.show()
-
-    return retour
+            # print(position.shape, velocity.shape, time_to_integrate_on.shape)
+            energy_recomb = 0.5 * velocity_recomb**2
+            # print(energy_recomb)
+            # plot the trajectory
+            np.clip(position, position_grid_for_plot[0], position_grid_for_plot[-1], out=position)
+            position = position[np.logical_and(time_to_integrate_on >= max(time_grid_for_plot[0], time_ionization), time_to_integrate_on <= time_grid_for_plot[-1])]
+            # print(time_grid_for_plot[np.logical_and(time_grid_for_plot>=time_ionization, time_grid_for_plot<=t_recombination)].shape, position.shape, time_grid_for_plot.shape)
+            ax.plot(time_grid_for_plot[np.logical_and(time_grid_for_plot>=time_ionization, time_grid_for_plot<=t_recombination)], position, alpha=0.7, color=cmap(norm(energy_recomb)), label=f'Ionization at {time_ionization:.2f} fs')
 
 
 if __name__ == "__main__":
+    # constants (DO NOT CHANGE THESE VALUES)
+    c = 2.99792458e8                                    # m/s, speed of light
+    e = 1.602176634e-19                                 # C, elementary charge
+    m = 9.10938356e-31                                  # kg, electron mass
+    a0 = 5.291772108e-11                                # m, Bohr radius
+    hbar = 1.0545718e-34                                # J.s, reduced Planck's constant
+    t_au = 2.418884e-17                                 # s, constant for conversion to atomic units
+    epsilon_0 = 8.854187817e-12                         # F/m, vacuum permittivity
+    E_h = 4.3597447222071e-18                           # J, Hartree energy
 
 
-    # Example usage
-    time_grid = np.linspace(-10, 10, 1000)*1e-15  # Time grid in seconds
-    position_grid = np.linspace(-15e-9, 15e-9, 100)  # Position grid in meters
+    fig, ax = plt.subplots(figsize=(10, 6))
 
-    def E_field(t):
-        """ Example electric field function. Replace with actual field calculation. """
-        wavelength = 800*1e-9      # m, NOT IN A.U. the conversion is done later
-        I_wcm2 = 1e14  # Intensity in W/cm^2, NOT IN A.U. the conversion is done later
-        omega = 2 * np.pi * c / wavelength  # angular frequency in rad/s
-        E0 = np.sqrt(I_wcm2 * 1e4 / (c*epsilon_0))  # electric field amplitude in V/m
-        return E0 * np.sin(omega * t)
+    dx = 0.01                                         # in a.u. => should be small enough to resolve well the wave function => dx < 0.1 is a good value
+    x = np.arange(-120, 120, dx)                     # in a.u. => 1au=roughly 24as, this is the space grid for the simulation
+    dt = 0.05                                           # in a:u , if dt>0.05, we can t see electron that comes back to the nucleus
+    t = np.arange(-1000, 1000, dt)                      # also in a.u. => 1au=roughly 24as
 
-    time_grid_zoomed = time_grid[250:750]  # Zoomed time grid (middle part)
-    position_grid_zoomed = position_grid[25:75]  # Zoomed position grid (middle part)
+    # Laser Parameters
+    wavelength = 800                                    # nm, NOT IN A.U. the conversion is done later
+    I_wcm2 = 1e14                                       # Intensity in W/cm^2, NOT IN A.U. the conversion is done later
+    freq = 3e8 / (wavelength * 1e-9)                    # Frequency in Hz, converting nm to m
+    omega_au = 2*np.pi*freq*t_au
+    periode_au = 2*np.pi / omega_au                     # Period in atomic units
+    pulse_duration = 25 * periode_au                    # Pulse duration in atomic units
+    E0_laser = np.sqrt(2/(epsilon_0*c)) * np.sqrt(I_wcm2*1e4) / (E_h/(e*a0))       # from intensity to eletric field in a.u.
+    champE_func = lambda t: E0_laser*np.cos(omega_au * t)
 
-    print("Generating mask from classical trajectories...")
-    mask = generate_mask_from_classical(time_grid, position_grid, E_field, time_grid_zoomed, position_grid_zoomed)
-    print("Mask generation complete.")
-    plt.imshow(mask, cmap='turbo', aspect='auto',
-               extent=(time_grid_zoomed[0], time_grid_zoomed[-1], position_grid_zoomed[0], position_grid_zoomed[-1]))
-    plt.colorbar(label='Mask Value')
-    plt.title("Generated Mask from Classical Trajectories")
+
+    plot_classical_trajectories(champE_func, t, ax, time_grid_for_plot=t[np.logical_and(t>=0, t<=750)], position_grid_for_plot=x)
     plt.show()
